255f5ce14fe374ca111b8d32d80e95f8bf67af85
Huxyshuu <hugotamm@hotmail.com>
2022-07-03 20:31:47 +0300

    Fixed log in menu looking wonky, hopefully mobile menu is good as well as admin list onclick

--- src/components/MobileMenu.jsx
+++ src/components/MobileMenu.jsx
@@ -11,25 +11,27 @@ export default function MobileMenu(prop) {
 
   return (
     <div id="dropdownMenu">
-      <div id="ddmenu">
-        <h2 id="menuTitle">Recipe Library</h2>
-        <h2 id="menuMenu">MENU</h2>
-      </div>
-      <div id="menuSection">
-        <p>Haku</p>
-        <div>
-          <input id="menuSearch" type="text" placeholder="Etsi reseptiä nimellä"/>
-          <div id="searchIconButton">
-            <Icon icon="bx:search-alt" />
+      <div>
+        <div id="ddmenu">
+          <h2 id="menuTitle">Recipe Library</h2>
+          <h2 id="menuMenu">MENU</h2>
+        </div>
+        <div id="menuSection">
+          <p>Haku</p>
+          <div>
+            <input id="menuSearch" type="text" placeholder="Etsi reseptiä nimellä"/>
+            <div id="searchIconButton">
+              <Icon icon="bx:search-alt" />
+            </div>
           </div>
-        </div>
 
-        <div className="menuButtons">
-          <Link to="/" className="link" onClick={() => setMenuOpen(false)}><button id="menuFrontButton">Etusivu</button></Link>
-          {loggedIn && <Link to="/overview" className="link" onClick={() => setMenuOpen(false)}><button id="menuOverviewButton">Hallinta</button></Link>}
-          <Link to={loggedIn ? '/' : '/login'} className="link" onClick={() => {setMenuOpen(false); if(loggedIn){setLoggedIn(false)}}}><button id="menuLoginButton">{loggedIn ? 'Kirjaudu ulos' : 'Kirjaudu'}</button></Link>
+          <div className="menuButtons">
+            <Link to="/" className="link" onClick={() => setMenuOpen(false)}><button id="menuFrontButton">Etusivu</button></Link>
+            {loggedIn && <Link to="/overview" className="link" onClick={() => setMenuOpen(false)}><button id="menuOverviewButton">Hallinta</button></Link>}
+            <Link to={loggedIn ? '/' : '/login'} className="link" onClick={() => {setMenuOpen(false); if(loggedIn){setLoggedIn(false)}}}><button id="menuLoginButton">{loggedIn ? 'Kirjaudu ulos' : 'Kirjaudu'}</button></Link>
+          </div>
+          
         </div>
-        
       </div>
       <div id="menuFooter">
         <small>Copyright &copy; 2022 RECLIB. All Right Reserved</small>
