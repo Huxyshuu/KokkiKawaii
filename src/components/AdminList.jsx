7dbcb4d604ef149c0fe3b360c417795dd1b3cd06
Huxyshuu <hugotamm@hotmail.com>
2022-07-15 11:31:59 +0300

    Added a success and failed prompt to deletion

--- src/components/AdminList.jsx
+++ src/components/AdminList.jsx
@@ -7,6 +7,9 @@ export default function AdminList(prop) {
 
   const [isPrompted, setIsPrompted] = useState(false);
   const [currentDeletion, setCurrentDeletion] = useState();
+  const [isDeleting, setIsDeleting] = useState(false);
+  const [isDeleted, setIsDeleted] = useState(false);
+  const [deleteFailed, setDeleteFailed] = useState(false);
   const [state, setState] = useState({
     isLoading: true,
     data: []
@@ -25,23 +28,37 @@ export default function AdminList(prop) {
     e.preventDefault();
     const text = document.getElementById('promptText');
     if(e.target[0].value === 'password') {
+      setIsDeleting(true);
+      axios.delete(backendURL + currentDeletion._id)
+        .then(response => {
+          setState(prevState => ({
+            ...prevState,
+            data: state.data.filter(e => e._id !== currentDeletion._id)
+          }));
+          setIsDeleting(false);
+          setIsDeleted(true);
+          text.innerHTML = '0';
+          // console.log(`Reseptti poistettiin: \n${currentDeletion.title} (id:${currentDeletion._id})`)
+          
+          setTimeout(() => {
+            setCurrentDeletion();
+            setIsDeleted(false);
+            setIsPrompted(false);
+          }, 3000)
+        })
+        .catch(err => {
+          setIsDeleting(false);
+          setIsDeleted(true)
+          text.innerHTML = '0';
+          setDeleteFailed(true);
 
-    axios.delete(backendURL + currentDeletion._id)
-      .then(response => {
-        setState(prevState => ({
-          ...prevState,
-          data: state.data.filter(e => e._id !== currentDeletion._id)
-        }));
-        console.log(`Reseptti poistettiin: \n${currentDeletion.title} (id:${currentDeletion._id})`)
-        setCurrentDeletion();
-        setIsPrompted(false);
-        text.innerHTML = '0';
-      })
-      .catch(err => {
-        console.error(err.message)
-      })
-
-      
+          setTimeout(() => {
+            setCurrentDeletion();
+            setIsDeleted(false);
+            setIsPrompted(false);
+            setDeleteFailed(false);
+          }, 10000)
+        })
     } else {
       text.innerHTML = 'Väärä salasana!';
     }
@@ -74,15 +91,46 @@ export default function AdminList(prop) {
       {
         isPrompted && 
         <div id="promptBox">
-          <p id="askPass" >Syötä salasana poistaakseen reseptti:<br/><span className="highlightColor">{currentDeletion.title}<br />(id: {currentDeletion._id})</span></p>
-          <form action="" onSubmit={deleteItem}>
-            <p id="promptText"></p>
-            <input type="password" placeholder="Salasana" id="promptPassword"/>
-            <div id="promptButtons">
-              <input id="promptX" type="button" value="SULJE" onClick={() => {setIsPrompted(false); setCurrentDeletion()}}/>
-              <input id="promptDel" type="submit" value="POISTA"/>
+          {
+            isDeleted ? 
+            <>
+            {
+              deleteFailed ? 
+              <>
+              <p id="askPass">Reseptin poisto epäonnistui. <br/>Yritä uudelleen myöhemmin tai ota yhteyttä ylläpitäjään <br/><br/><span className="highlightColor">hugotamm@hotmail.com</span></p>
+              </>
+              :
+              <>
+              <p id="askPass">Resepti: <span className="highlightColor">{currentDeletion.title}<br />(id: {currentDeletion._id})</span><br /> on poistettu</p>
+              </>
+            }
+            </>
+            :
+            <>
+            {
+            isDeleting ? 
+            <>
+            <div id="adminlistLoader">
+              <div id="adminlistLoadingSpinner">
+              </div> 
+              <h3>Poistetaan...</h3>
             </div>
-          </form>
+            </>
+            :
+            <>
+            <p id="askPass" >Syötä salasana poistaakseen reseptti: <span className="highlightColor">{currentDeletion.title}<br />(id: {currentDeletion._id})</span></p>
+            <form action="" onSubmit={deleteItem}>
+              <p id="promptText"></p>
+              <input type="password" placeholder="Salasana" id="promptPassword"/>
+              <div id="promptButtons">
+                <input id="promptX" type="button" value="SULJE" onClick={() => {setIsPrompted(false); setCurrentDeletion()}}/>
+                <input id="promptDel" type="submit" value="POISTA"/>
+              </div>
+            </form>
+            </>
+            }
+            </>
+          }
         </div>
       }
       {
